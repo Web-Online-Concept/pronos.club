cdac2f3471fb10c7083c4142b1931180a24d44fb
Web-Online-Concept <web.online.concept@gmail.com>
2026-03-27 07:33:57 +0100

    modifs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,17 @@
 import "@/app/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+};
 
 export default function RootLayout({
   children,
