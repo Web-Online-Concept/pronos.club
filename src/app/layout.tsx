01089a4b4aef17ede58b5eff836002bee5eb5c91
Web-Online-Concept <web.online.concept@gmail.com>
2026-03-31 02:28:16 +0200

    modifs SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import "@/app/globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
+
+const GA_ID = "G-EH0DSCDKGR";
 
 export const metadata: Metadata = {
   icons: {
@@ -20,6 +23,23 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        {/* Google Search Console */}
+        <meta name="google-site-verification" content="iINiCbpros1aKuWwQVc0ug4xsIVR6PQtXGYCon_9bnY" />
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+      </head>
       <body className="antialiased">
         {children}
       </body>
