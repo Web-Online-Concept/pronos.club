9b776cf2b6b29207f020a9c25d06d2fa487ef727
Web-Online-Concept <web.online.concept@gmail.com>
2026-04-19 12:12:30 +0200

    Re-enable hydration debugger to find real cause (not Turbopack)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { routing } from "@/i18n/routing";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import { defaultOpenGraph, defaultTwitter } from "@/lib/seo";
 import MobileBottomBar from "@/components/layout/MobileBottomBar";
+import HydrationDebugger from "@/components/debug/HydrationDebugger";
 
 export async function generateMetadata({
   params,
@@ -66,6 +67,7 @@ export default async function LocaleLayout({
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       <AuthProvider>
+        <HydrationDebugger />
         {children}
         <MobileBottomBar />
       </AuthProvider>
